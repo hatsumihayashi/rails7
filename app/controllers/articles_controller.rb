32594bbaec96014ce117c2e67efb7ff44942c5d5
Hatsumi Hayashi <hayashihatsumi0114@gmail.com>
2022-08-28 19:00:28 +0900

    [チュートリアル] 新規登録フォームの内容でArticleを登録する(Create)ページを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
